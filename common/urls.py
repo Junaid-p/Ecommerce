b9acf254135d660d11ed370ff22b405e266cf07d
Junaid-p <786muhammedjunaid@gmail.com>
2022-09-26 10:27:00 +0530

    updation 26/09/2022

--- common/urls.py
+++ common/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-
+app_name="common"
 urlpatterns=[
     path('',views.common_index),
-    path('changepassword',views.change_password),
+    path('changepassword',views.change_password,),
     path('customerlogin',views.customer_login),
-    path('customersignup',views.customer_signup),
+    path('customersignup',views.customer_signup, name='customersig'),
     path('sellerlogin',views.seller_login),
     path('sellersignup',views.seller_signup),
     path('imgview',views.image_view)
